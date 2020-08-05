90f8dc73824e80c3a442c08049ca569d090f006e
Andre Lima <a.s.l0@hotmail.com>
2020-08-05 18:12:18 -0300

    fetch api distro

---
+++ js/modules/fetch-distro.js
@@ -0,0 +1,27 @@
+import initAnimaNumeros from "./anima-numeros.js";
+
+export default function initFetchDistro() {
+
+    async function fetchDistro (url) {
+        const distroResponse = await fetch(url);
+        const distroJSON = await distroResponse.json();
+        const numeroGrid = document.querySelector('.numeros-grid');
+        distroJSON.forEach(distro =>{
+            const divDistro = createDistro(distro);
+            numeroGrid.appendChild(divDistro);
+        });
+        initAnimaNumeros();
+    }
+    
+    function createDistro(distro) {
+        const div = document.createElement('div');
+        div.classList.add('numero-usuario');
+        div.innerHTML = `<h3>${distro.name}</h3><span data-numero>${distro.total}</span>`;
+        return div;
+    
+    }
+    
+    fetchDistro('./distrosapi.json');
+
+}
+
